a94cb7581181b4b511fce96d1a431fbb38be2c18
ricardotenorio <ricardo.heber@gmail.com>
2021-07-23 09:03:40 -0300

    links para index.html e user_profile.html

--- script/template/renderTemplate.js
+++ script/template/renderTemplate.js
@@ -2,18 +2,27 @@ const header = document.querySelector('#main-header');
 const searchBox = document.querySelector('#search-box');
 
 let username = localStorage["name"];
-const links = `<a href="login.html">entrar</a>
-  /
-  <a href="signup.html">cadastro</a>`
+const linkLogin = `<a href="login.html">entrar</a> -
+  <a href="signup.html">cadastro</a>`;
+
+const linkProfile = `
+  <a href="./user_profile.html">
+    ${username}
+  </a>
+`;
 
 const renderHeader = (elem) => {
-  const content = `<h3 class="site-title">Reservas</h3>
-        
+  const content = `
+                  <a href="./index.html">
+                    <h3 class="site-title">
+                      Reservas
+                    </h3>
+                  </a>
                   <div class="logged-user">
                       <img src="img/avatar.png" alt="logo usuário">
 
                       <div class="auth-links">
-                          ${username ? username : links}
+                          ${username ? linkProfile : linkLogin}
                       </div>
                   </div>`;
 
